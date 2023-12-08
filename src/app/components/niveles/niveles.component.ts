16942db00199667355d94e3b02303f130d6dc56f
Jesus8556 <luis.m.gutierrezecsup.edu.pe>
2023-12-07 23:16:33 -0500

    act

--- src/app/components/niveles/niveles.component.ts
+++ src/app/components/niveles/niveles.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NivelesService } from 'src/app/services/niveles.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; // Importa ReactiveFormsModule si no lo has hecho
+
 
 @Component({
   selector: 'app-niveles',
@@ -9,9 +11,18 @@ import { NivelesService } from 'src/app/services/niveles.service';
 })
 export class NivelesComponent implements OnInit {
   niveles: any[] = [];
+  nivelForm: FormGroup;
+  selectedNivelId: string | null = null;
 
-  constructor(private route: ActivatedRoute, private nivelesService: NivelesService) {}
-
+  constructor(private route: ActivatedRoute, private nivelesService: NivelesService, private fb: FormBuilder) {
+    this.nivelForm = this.fb.group({
+      // Define los campos de tu formulario
+      nivel: ['', Validators.required],
+      empresa: [''], // Agregar empresa como control si aún no lo has hecho
+      imagen: ['']
+      // Otros campos según tus necesidades
+    });
+  }
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       const empresaId = params.get('id');
@@ -25,6 +36,59 @@ export class NivelesComponent implements OnInit {
         this.niveles = niveles;
       });
     }
+  }
+  loadNivelDetails(nivelId: string): void {
+    this.nivelesService.getNivelById(nivelId).subscribe((nivel) => {
+      // Llenar el formulario con los detalles del nivel
+      this.nivelForm.patchValue({
+        nivel: nivel.nivel,
+        empresa: nivel.empresa.nombre, // Ajusta esto según la estructura real de tu objeto nivel. Si empresa es un objeto, puedes acceder a su propiedad nombre así.
+        imagen: nivel.imagen
+        // Otros campos según tus necesidades
+      });
+
+      this.selectedNivelId = nivelId;
+    });
+  }
+
+
+  // Método para enviar datos del formulario y realizar operaciones CRUD
+  submitNivelForm(): void {
+    const nivelData = this.nivelForm.value;
+
+    if (this.selectedNivelId) {
+      // Actualizar nivel existente
+      this.nivelesService.updateNivel(this.selectedNivelId, nivelData).subscribe(() => {
+        // Recargar la lista de niveles después de la actualización
+        this.getNivelesByEmpresaId(this.route.snapshot.paramMap.get('id'));
+        // Restablecer el formulario
+        this.resetForm();
+      });
+    } else {
+      // Crear un nuevo nivel
+      this.nivelesService.createNivel(nivelData).subscribe(() => {
+        // Recargar la lista de niveles después de la creación
+        this.getNivelesByEmpresaId(this.route.snapshot.paramMap.get('id'));
+        // Restablecer el formulario
+        this.resetForm();
+      });
+    }
+  }
+
+  // Método para eliminar un nivel
+  deleteNivel(nivelId: string): void {
+    this.nivelesService.deleteNivel(nivelId).subscribe(() => {
+      // Recargar la lista de niveles después de la eliminación
+      this.getNivelesByEmpresaId(this.route.snapshot.paramMap.get('id'));
+      // Restablecer el formulario
+      this.resetForm();
+    });
+  }
+
+  // Método para restablecer el formulario
+  resetForm(): void {
+    this.nivelForm.reset();
+    this.selectedNivelId = null;
   }
 }
 
--- src/app/services/niveles.service.ts
+++ src/app/services/niveles.service.ts
@@ -16,4 +16,26 @@ export class NivelesService {
     const url = `${this.apiUrl}?empresaId=${empresaId}`;
     return this.http.get<any[]>(url);
   }
+    // Crear un nuevo nivel
+    createNivel(nivelData: any): Observable<any> {
+      return this.http.post<any>(this.apiUrl, nivelData);
+    }
+  
+    // Obtener un nivel por ID
+    getNivelById(nivelId: string): Observable<any> {
+      const url = `${this.apiUrl}/${nivelId}`;
+      return this.http.get<any>(url);
+    }
+  
+    // Actualizar un nivel por ID
+    updateNivel(nivelId: string, nivelData: any): Observable<any> {
+      const url = `${this.apiUrl}/${nivelId}`;
+      return this.http.put<any>(url, nivelData);
+    }
+  
+    // Eliminar un nivel por ID
+    deleteNivel(nivelId: string): Observable<any> {
+      const url = `${this.apiUrl}/${nivelId}`;
+      return this.http.delete<any>(url);
+    }
 }
